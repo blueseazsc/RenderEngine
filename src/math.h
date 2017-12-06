7272127d084713c50e189b1a3f3a3038a5761a87
blueseazsc <bluesea.zhangsc@icloud.com>
2017-12-06 19:17:24 +0800

    add colorLerp

--- src/math.h
+++ src/math.h
@@ -46,5 +46,15 @@ public:
 	uint8 _a;
 };
 typedef Rgba4Byte Rgba;
+
+inline Rgba4Byte colorLerp(const Rgba4Byte& c1, const Rgba4Byte& c2, float s)
+{
+	Rgba4Byte color;
+	color._r = (uint8)(c1._r + s * ( c2._r - c1._r ));
+	color._g = (uint8)(c1._g + s * ( c2._g - c1._g ));
+	color._b = (uint8)(c1._b + s * ( c2._b - c1._b ));
+	color._a = (uint8)(c1._a + s * ( c2._a - c1._a ));
+	return color;
+}
 }
 #endif
